21a4cf0b2d8c5851771c7500fed8c9cac428f4b8
falahaboud <falah.aboud@gmail.com>
2019-03-10 23:41:10 +0100

    Secrt page gemacht

--- mamakocht/core/views.py
+++ mamakocht/core/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+
 
 def home(request):
-    return render(request, 'home.html')
+    count = User.objects.count()
+    return render(request, 'home.html', {
+        'count': count
+    })
 
 def singup(request):
-    return render(request, 'registration/singup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    else:
+        form = UserCreationForm()
+
+    form = UserCreationForm()
+    return render(request, 'registration/singup.html', {
+        'form': form
+    })
+@login_required
+def secret_page(request):
+    return render(request, 'secret_page.html')
+
+
+class SecretPage(TemplateView):
+    template_name = 'secret_page.html'
